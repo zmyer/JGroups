0a413691be3f5efbc1cdee153712d7e7cbe7de87
Bela Ban <belaban@yahoo.com>
2009-02-20 11:35:31 +0000

    added

---
+++ src/org/jgroups/PhysicalAddress.java
@@ -0,0 +1,9 @@
+package org.jgroups;
+
+/**
+ * Represents a physical (as opposed to logical) address
+ * @author Bela Ban
+ * @version $Id: PhysicalAddress.java,v 1.1.2.1 2009/02/20 11:35:31 belaban Exp $
+ */
+public interface PhysicalAddress extends Address {
+}
