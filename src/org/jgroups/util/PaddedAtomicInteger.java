c8754b7ca9daedf76f43195d6f6ed9f2ea7e9251
Bela Ban <belaban@yahoo.com>
2016-06-30 13:29:53 +0200

    ns

--- src/org/jgroups/util/PaddedAtomicInteger.java
+++ src/org/jgroups/util/PaddedAtomicInteger.java
@@ -8,8 +8,8 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 @SuppressWarnings("serial")
 public class PaddedAtomicInteger extends AtomicInteger {
-    protected volatile int i1,i2,i3,i4,i5,i6=1;
-    protected volatile int i7,i8,i9,i10,i11,i12,i13,i14=2;
+    protected volatile int i1=1,i2=2,i3=3,i4=4,i5=5,i6=6;
+    protected volatile int i7=7,i8=8,i9=9,i10=10,i11=11,i12=12,i13=13,i14=14;
 
     public PaddedAtomicInteger(int initialValue) {
         super(initialValue);
--- src/org/jgroups/util/PaddedAtomicLong.java
+++ src/org/jgroups/util/PaddedAtomicLong.java
@@ -10,7 +10,7 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 @SuppressWarnings("serial")
 public class PaddedAtomicLong extends AtomicLong {
-    public volatile long p1,p2,p3,p4,p5,p6,p7=1L;
+    public volatile long p1=1,p2=2,p3=3,p4=4,p5=5,p6=6,p7=7;
 
     public long sum() { // prevents optimizing away the fields above
         return p1+p2+p3+p4+p5+p6+p7;
